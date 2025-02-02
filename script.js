825308b3b1497ce6e4e604030e96e2f362225bba
boris2442 <aubinborissimotsebo@gmail.com>
2025-02-02 12:22:14 +0100

    text

--- script.js
+++ script.js
@@ -21,15 +21,19 @@ let col3= Math.floor( Math.random()*255);
 let col4= Math.floor( Math.random()*255);
 let col5= Math.floor( Math.random()*255);
 let col6= Math.floor( Math.random()*255);
-console.log(col1, col2)
+let col7= Math.floor( Math.random()*255);
+let col8= Math.floor( Math.random()*255);
+let col9= Math.floor( Math.random()*255);
+let col10= Math.floor( Math.random()*255);
+let col11= Math.floor( Math.random()*255);
+let col12= Math.floor( Math.random()*255);
+
 let angle=Math.floor(Math.random()*360);
 
 
 document.body.style.background=`linear-gradient( ${angle}deg, rgb(${col1}, ${col2}, ${col3}), rgb(${col4}, ${col5}, ${col6}))`
 
-code.textContent=`linear-gradient( ${angle}deg, rgb(${col1}, ${col2}, ${col3}), rgb(${col4}, ${col5}, ${col6}))`;
+code.textContent=`linear-gradient( ${angle}deg, rgb(${col1}, ${col2}, ${col3}), rgb(${col4}, ${col5}, ${col6}),  rgb(${col7}, ${col8}, ${col8}),  rgb(${col10}, ${col11}, ${col12}))`;
 
 }
 btn.addEventListener("click", linearGradient )
-// 
-// )
